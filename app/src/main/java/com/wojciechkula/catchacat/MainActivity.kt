44ddda69dd8094e9253b93374171b173c9e446f5
Wojciech Kula <wojtekkula33@gmail.com>
2022-05-13 13:01:42 +0200

    CAC-10: add splash screen

--- app/src/main/java/com/wojciechkula/catchacat/MainActivity.kt
+++ app/src/main/java/com/wojciechkula/catchacat/MainActivity.kt
@@ -2,12 +2,14 @@ package com.wojciechkula.catchacat
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         setContentView(R.layout.activity_main)
     }
 }
